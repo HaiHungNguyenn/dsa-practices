d398a4bb82ee97f53c766288492d1e45f0999556
Nguyễn Hưng Hải <116075009+HaiHungNguyenn@users.noreply.github.com>
2026-04-11 13:58:22 +0700

    Add: buy-and-sell-crypto - submission-1

---
+++ Data Structures & Algorithms/buy-and-sell-crypto/submission-1.cs
@@ -0,0 +1,16 @@
+public class Solution {
+    public int MaxProfit(int[] prices) {
+        int maxProfit = 0;
+        for (int i = 0; i < prices.Length; i ++)
+        {
+            for (int j = i + 1; j < prices.Length; j ++)
+            {
+                if (maxProfit < (prices[j] - prices[i]))
+                {
+                    maxProfit = prices[j] - prices[i];
+                }
+            }
+        }
+        return maxProfit;
+    }
+}
