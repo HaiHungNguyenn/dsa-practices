f45fb6036539a251d52b60dfcd984e48335ca46a
Nguyễn Hưng Hải <116075009+HaiHungNguyenn@users.noreply.github.com>
2026-04-11 16:05:19 +0700

    Add: can-place-flowers - submission-3

---
+++ Data Structures & Algorithms/can-place-flowers/submission-3.cs
@@ -0,0 +1,19 @@
+public class Solution {
+    public bool CanPlaceFlowers(int[] flowerbed, int n) {
+        int canPlaceCount = 0;
+        int[] newBed = [0 , ..flowerbed, 0];
+        for (int i = 1; i < newBed.Length - 1; i ++)
+        {
+            if (newBed[i] == 0 && newBed[i - 1] == 0 && newBed[i + 1] == 0)
+            {
+                newBed[i] = 1;
+                canPlaceCount ++;
+            }
+
+            if (canPlaceCount == n)
+                break;
+        }
+
+        return canPlaceCount == n || canPlaceCount > n;
+    }
+}
